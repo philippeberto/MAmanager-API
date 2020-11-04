333966568e1ae09ccba7a56485d65d1bec774d2f
Philippe <philippe.bjj@gmail.com>
2020-11-04 14:14:32 +0000

    add sclarar Date
    Adição do scalar Date mas ainda não utilizado.

--- graphql/resolvers/despesas.js
+++ graphql/resolvers/despesas.js
@@ -16,10 +16,36 @@ const findAllDespesas = async (parent, { user }) => {
   }
 }
 
+const findAllDespesasByDate = async (parent, { user, dates }) => {
+  try {
+    const despesasDB = db.collection(user).doc('Data').collection('Despesas')
+      .where('date', '>=', dates.idate)
+      .where('date', '<=', dates.fdate)
+      .get();
+    if (despesasDB.empty) {
+      console.log('No matching documents.');
+      return [];
+    } else {
+      const despesas = []
+      despesasDB.forEach((doc) => {
+        despesas.push({
+          ...doc.data(),
+          id: doc.id,
+        })
+      })
+      return despesas
+    }
+  } catch (err) {
+    console.log('Error getting documents', err);
+    return []
+  }
+}
+
 const createDespesa = async (parent, { user, input }) => {
   console.log(input)
   const doc = db.collection(user).doc('Data').collection('Despesas').doc()
   await doc.set(input)
+  // await doc.set({ createdAt: new Date() })
   const id = doc.id
   return { id, ...input }
 }
@@ -27,4 +53,5 @@ const createDespesa = async (parent, { user, input }) => {
 module.exports = {
   findAllDespesas,
   createDespesa,
+  findAllDespesasByDate
 }
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,4 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express')
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 
 const {
   findAllAlunos,
@@ -8,10 +10,11 @@ const {
   updateAluno,
 } = require('./alunos')
 const { findAllMensalidades, createMensalidade } = require('./mensalidades')
-const { findAllDespesas, createDespesa } = require('./despesas')
+const { findAllDespesas, createDespesa, findAllDespesasByDate } = require('./despesas')
 const { findAllVendas, createVenda } = require('./vendas')
 const { findAllCompras, createCompra } = require('./compras')
-const { findAllSeguros, createSeguro } = require('./seguros')
+const { findAllSeguros, createSeguro } = require('./seguros');
+const moment = require('moment');
 
 const needsAuth = (resolver) => {
   return async (parent, args, context) => {
@@ -28,6 +31,7 @@ const resolvers = {
     findAluno: needsAuth(findAluno),
     findAllMensalidades: needsAuth(findAllMensalidades),
     findAllDespesas: needsAuth(findAllDespesas),
+    findAllDespesasByDate: needsAuth(findAllDespesasByDate),
     findAllVendas: needsAuth(findAllVendas),
     findAllCompras: needsAuth(findAllCompras),
     findAllSeguros: needsAuth(findAllSeguros),
@@ -42,6 +46,23 @@ const resolvers = {
     createCompra: needsAuth(createCompra),
     createSeguro: needsAuth(createSeguro),
   },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue(value) {
+      return moment(value); // value from the client
+    },
+    serialize(value) {
+      console.log(value)
+      return moment(value).format("DD/MM/YYYY"); // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.STRING) {
+        return moment(+ast.value) // ast value is always in string format
+      }
+      return null;
+    },
+  })
 }
 
 module.exports = resolvers
