927bcc4b8cb39833b08d0f762a187d689c61441b
Philippe <philippe.bjj@gmail.com>
2020-11-05 13:04:18 +0000

    ajustes nas variáveis de ambiente

--- firestore/index.js
+++ firestore/index.js
@@ -1,12 +1,29 @@
 const admin = require('firebase-admin')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const serviceAccount = require('../firestore.json')
+const fromBase64 = value => {
+  const buff = new Buffer.from(value, 'base64')
+  return buff.toString('ascii')
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://gym-bd.firebaseio.com'
+  credential: admin.credential.cert({
+    type: process.env.FIRESTORE_TYPE,
+    project_id: process.env.FIRESTORE_PROJECT_ID,
+    private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
+    private_key: fromBase64(process.env.FIRESTORE_PRIVATE_KEY),
+    client_email: process.env.FIRESTORE_CLIENT_EMAIL,
+    client_id: process.env.FIRESTORE_CLIENT_ID,
+    auth_uri: process.env.FIRESTORE_AUTH_URI,
+    token_uri: process.env.FIRESTORE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIRESTORE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIRESTORE_CLIENT_X509_CERT_URL
+  }),
+  databaseURL: process.env.FIRESTORE_DATABASE_URL
 })
 
 const db = admin.firestore()
+
 
 module.exports = db
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3001
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT
 const graphqlServer = require('./graphql')
 
 
 graphqlServer.applyMiddleware({ app })
+
+const fromBase64 = value => {
+  const buff = new Buffer.from(value, 'base64')
+  return buff.toString('ascii')
+}
 
 app.listen(PORT, (err) => {
   if (err) {
